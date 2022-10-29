f3be279f2d6e2169f492b7ce752b5ab18f2aa031
Emmanuel Oloyede <emmanueltopea@gmail.com>
2022-10-29 03:47:24 +0100

    feat(config): add passport authenticator to signup route

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const authController = require("../controllers/authController")
+const passport = require("passport");
 
 const router = express.Router();
 
-router.post("/signup", authController.signup_post)
+router.post("/signup", passport.authenticate('signup', {session: false}), authController.signup_post)
 
 router.post("/login", authController.login_post)
 
