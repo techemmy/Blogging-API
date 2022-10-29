53822cfa30ba17f925c42027c31d5d1ec997b477
Emmanuel Oloyede <emmanueltopea@gmail.com>
2022-10-29 04:18:44 +0100

    feat(config): add passport authenticator to login route

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -6,6 +6,14 @@ const router = express.Router();
 
 router.post("/signup", passport.authenticate('signup', {session: false}), authController.signup_post)
 
-router.post("/login", authController.login_post)
+router.post("/login", async (req, res, next) => {
+    passport.authenticate('login', {failureRedirect: '/sike'}, (error, user, info) => {
+        try {
+            authController.login_post(req, res, next, user, info);
+        } catch (error) {
+            next(error);
+        }
+    })(req, res, next)
+})
 
 module.exports = router;
