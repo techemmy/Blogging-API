8acf47fad8058613567a9682787ddb4f610230c2
Emmanuel Oloyede <emmanueltopea@gmail.com>
2022-10-29 03:51:55 +0100

    feat: add passport strategy for signup

--- middleware/passport.js
+++ middleware/passport.js
@@ -1,7 +1,27 @@
 const passport = require("passport");
+const User = require("../models/user");
+const localStrategy = require("passport-local").Strategy
 const JWTStrategy = require("passport-jwt").Strategy
 const ExtractJwt = require("passport-jwt").ExtractJwt
 require("dotenv").config()
+
+
+passport.use('signup',
+    new localStrategy({
+        usernameField: 'email',
+        passwordField: 'password',
+        passReqToCallback: true
+    }, async (req, email, password, done) => {
+        try {
+            const {firstName, lastName} = req.body
+            const user = await User.create({ firstName, lastName, email, password})
+            return done(null, user)
+        } catch (error) {
+            console.log(error);
+            done(error);
+        }
+    })
+)
 
 passport.use(
     new JWTStrategy({
