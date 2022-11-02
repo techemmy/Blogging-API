70ab45063c5e65b1ad86ae8a7238a6edfcf0f80a
Emmanuel Oloyede <emmanueltopea@gmail.com>
2022-11-02 05:52:44 +0100

    refactor: remove unused functionality

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -7,7 +7,7 @@ const router = express.Router();
 router.post("/signup", passport.authenticate('signup', {session: false}), authController.signup_post)
 
 router.post("/login", async (req, res, next) => {
-    passport.authenticate('login', {failureRedirect: '/sike'}, (error, user, info) => {
+    passport.authenticate('login', (error, user, info) => {
         try {
             authController.login_post(error, req, res, next, user, info);
         } catch (error) {
