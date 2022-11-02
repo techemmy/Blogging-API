f77b62ee4938e4e314652f441c681d47349a28e9
Emmanuel Oloyede <emmanueltopea@gmail.com>
2022-11-02 04:42:36 +0100

    feat: implement get all published blogs

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -1,8 +1,12 @@
-const Blog = require("../models/blog");
+const {Blog, blogStates} = require("../models/blog");
 
-const getAllPublishedBlogs = (req, res) => {
-
-    res.status(200).json({blogs: "All"})
+const getAllPublishedBlogs = async (req, res) => {
+    try {
+        const blogs = await Blog.find({state: blogStates.published})
+        res.status(200).json({blogs: blogs})
+    } catch (error) {
+        next(error);
+    }
 }
 
 module.exports = {
