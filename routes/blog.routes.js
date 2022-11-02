ac132645b0f02dfeb187e6a8d495d31f671acbaa
Emmanuel Oloyede <emmanueltopea@gmail.com>
2022-11-02 05:25:09 +0100

    fix: add const to router variable to correct variable assignment

--- routes/blog.routes.js
+++ routes/blog.routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const authController = require("../controllers/blogController");
 
 
-router = express.Router();
+const router = express.Router();
 
 router.get("/", authController.getAllPublishedBlogs);
 router.get("/:id", authController.getPublishedBlogById);
