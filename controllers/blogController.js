037832f71980d3cfa618acfa8ce5b29df9722b62
Emmanuel Oloyede <emmanueltopea@gmail.com>
2022-11-02 05:15:49 +0100

    feat: implement get published blog by id

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -1,6 +1,7 @@
 const {Blog, blogStates} = require("../models/blog");
+const { isValidObjectId } = require("../utils");
 
-const getAllPublishedBlogs = async (req, res) => {
+const getAllPublishedBlogs = async (req, res, next) => {
     try {
         const blogs = await Blog.find({state: blogStates.published})
         res.status(200).json({blogs: blogs})
@@ -9,6 +10,24 @@ const getAllPublishedBlogs = async (req, res) => {
     }
 }
 
+const getPublishedBlogById = async (req, res, next) => {
+    try {
+        const blogId = req.params.id;
+
+        if (!isValidObjectId(blogId)) {
+            const error =  Error("Invalid blog id")
+            error.status = 400, error.statusCode = 400;
+            throw error
+        }
+
+        const blog = await Blog.findById(blogId)
+        res.status(200).json({blog})
+    } catch (error) {
+        next(error);
+    }
+}
+
 module.exports = {
-    getAllPublishedBlogs
+    getAllPublishedBlogs,
+    getPublishedBlogById
 }
