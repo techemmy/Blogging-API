c1338acaf1a784b94e68d13d321b2400b9abf7c3
Emmanuel Oloyede <emmanueltopea@gmail.com>
2022-11-03 02:39:11 +0100

    refactor: rename endpoint to be more descriptive

--- routes/blog.routes.js
+++ routes/blog.routes.js
@@ -8,7 +8,7 @@ const router = express.Router();
 router.get("/", authController.getAllPublishedBlogs_get);
 router.post("/", passport.authenticate('jwt', {session: false}), authController.createBlog_post);
 router.get("/:id", authController.getPublishedBlogById_get);
-router.patch("/state/:id", passport.authenticate('jwt', {session: false}), authController.updateBlogToPublish_patch);
+router.patch("/publish/:id", passport.authenticate('jwt', {session: false}), authController.updateBlogToPublish_patch);
 router.put("/:id", passport.authenticate('jwt', {session: false}), authController.editBlog_put);
 
 module.exports = router;
