e3401fbed821b8e0324c79ce78e71e13622314db
Emmanuel Oloyede <emmanueltopea@gmail.com>
2022-11-04 20:03:06 +0100

    test(fixtures): add more fixtures for blog tests

--- tests/fixtures/blog.js
+++ tests/fixtures/blog.js
@@ -12,9 +12,57 @@ module.exports = {
         tags: ["test1", "integration1"],
     },
     valid3: {
-        title: "Never",
+        title: "Blog 1",
         description: "Test blog fixture Test blog fixture Test blog fixture Test blog fixture Test blog fixture",
         body: "Testing blog data",
         tags: ["test2", "integration2"]
-    }
+    },
+    valid4: {
+        title: "Blog 2",
+        description: "Test blog fixture Test blog fixture Test blog fixture Test blog fixture Test blog fixture",
+        body: "Testing blog data",
+        tags: ["test2", "integration2"]
+    },
+    valid5: {
+        title: "Blog 3",
+        description: "Test blog fixture Test blog fixture Test blog fixture Test blog fixture Test blog fixture",
+        body: "Testing blog data",
+        tags: ["test2", "integration2"]
+    },
+    valid6: {
+        title: "Blog 4",
+        description: "Test blog fixture Test blog fixture Test blog fixture Test blog fixture Test blog fixture",
+        body: "Testing blog data",
+        tags: ["test2", "integration2"]
+    },
+    valid7: {
+        title: "Blog 5",
+        description: "Test blog fixture Test blog fixture Test blog fixture Test blog fixture Test blog fixture",
+        body: "Testing blog data",
+        tags: ["test2", "integration2"]
+    },
+    valid8: {
+        title: "Blog 6",
+        description: "Test blog fixture Test blog fixture Test blog fixture Test blog fixture Test blog fixture",
+        body: "Testing blog data",
+        tags: ["test2", "integration2"]
+    },
+    valid9: {
+        title: "Blog 7",
+        description: "Test blog fixture Test blog fixture Test blog fixture Test blog fixture Test blog fixture",
+        body: "Testing blog data",
+        tags: ["test2", "integration2"]
+    },
+    valid10: {
+        title: "Blog 8",
+        description: "Test blog fixture Test blog fixture Test blog fixture Test blog fixture Test blog fixture",
+        body: "Testing blog data",
+        tags: ["test2", "integration2"]
+    },
+    valid11: {
+        title: "Blog 9",
+        description: "Test blog fixture Test blog fixture Test blog fixture Test blog fixture Test blog fixture",
+        body: "Testing blog data",
+        tags: ["test2", "integration2"]
+    },
 }
