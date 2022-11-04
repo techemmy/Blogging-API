18c071c65c44a9ce9fc95de18bb1058a577ce5fb
Emmanuel Oloyede <emmanueltopea@gmail.com>
2022-11-04 14:28:09 +0100

    test: write test for application startup

---
+++ tests/integration/app.test.js
@@ -0,0 +1,21 @@
+const supertest = require("supertest");
+const app = require("../..");
+const database = require("../database");
+
+
+beforeAll(async () => {
+    await database.connect();
+})
+
+afterAll(async () => {
+    await database.disconnect();
+})
+
+
+describe("Application works", () => {
+    it("should get the homepage route '/' request successfully", async () => {
+        const request = await supertest(app).get("/");
+        expect(request.status).toBe(200);
+        expect(request.headers['content-type']).toContain("application/json");
+    })
+});
