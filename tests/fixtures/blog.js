67897085eb167a26d8e46e32cb0e083117ae3514
Emmanuel Oloyede <emmanueltopea@gmail.com>
2022-11-04 20:43:30 +0100

    style: rename some blogs title

--- tests/fixtures/blog.js
+++ tests/fixtures/blog.js
@@ -12,73 +12,73 @@ module.exports = {
         tags: ["test1", "integration1"],
     },
     valid3: {
-        title: "Blog 1",
-        description: "Test blog fixture Test blog fixture Test blog fixture Test blog fixture Test blog fixture",
-        body: "Testing blog data",
-        tags: ["test2", "integration2"]
-    },
-    valid4: {
-        title: "Blog 2",
-        description: "Test blog fixture Test blog fixture Test blog fixture Test blog fixture Test blog fixture",
-        body: "Testing blog data",
-        tags: ["test2", "integration2"]
-    },
-    valid5: {
         title: "Blog 3",
         description: "Test blog fixture Test blog fixture Test blog fixture Test blog fixture Test blog fixture",
         body: "Testing blog data",
         tags: ["test2", "integration2"]
     },
-    valid6: {
+    valid4: {
         title: "Blog 4",
         description: "Test blog fixture Test blog fixture Test blog fixture Test blog fixture Test blog fixture",
         body: "Testing blog data",
         tags: ["test2", "integration2"]
     },
-    valid7: {
+    valid5: {
         title: "Blog 5",
         description: "Test blog fixture Test blog fixture Test blog fixture Test blog fixture Test blog fixture",
         body: "Testing blog data",
         tags: ["test2", "integration2"]
     },
-    valid8: {
+    valid6: {
         title: "Blog 6",
         description: "Test blog fixture Test blog fixture Test blog fixture Test blog fixture Test blog fixture",
         body: "Testing blog data",
         tags: ["test2", "integration2"]
     },
-    valid9: {
+    valid7: {
         title: "Blog 7",
         description: "Test blog fixture Test blog fixture Test blog fixture Test blog fixture Test blog fixture",
         body: "Testing blog data",
         tags: ["test2", "integration2"]
     },
-    valid10: {
+    valid8: {
         title: "Blog 8",
         description: "Test blog fixture Test blog fixture Test blog fixture Test blog fixture Test blog fixture",
         body: "Testing blog data",
         tags: ["test2", "integration2"]
     },
-    valid11: {
+    valid9: {
         title: "Blog 9",
+        description: "Test blog fixture Test blog fixture Test blog fixture Test blog fixture Test blog fixture",
+        body: "Testing blog data",
+        tags: ["test2", "integration2"]
+    },
+    valid10: {
+        title: "Blog 10",
+        description: "Test blog fixture Test blog fixture Test blog fixture Test blog fixture Test blog fixture",
+        body: "Testing blog data",
+        tags: ["test2", "integration2"]
+    },
+    valid11: {
+        title: "Blog 11",
         description: "Test blog fixture Test blog fixture Test blog fixture Test blog fixture Test blog fixture",
         body: "Testing blog data",
         tags: ["test2", "integration2"]
     },
     valid12: {
-        title: "Blog 9",
+        title: "Blog 12",
         description: "Test blog fixture Test blog fixture Test blog fixture Test blog fixture Test blog fixture",
         body: "Testing blog data",
         tags: ["test2", "integration2"]
     },
     valid13: {
-        title: "Blog 9",
+        title: "Blog 13",
         description: "Test blog fixture Test blog fixture Test blog fixture Test blog fixture Test blog fixture",
         body: "Testing blog data",
         tags: ["test2", "integration2"]
     },
     valid14: {
-        title: "Blog 9",
+        title: "Blog 14",
         description: "Test blog fixture Test blog fixture Test blog fixture Test blog fixture Test blog fixture",
         body: "Testing blog data",
         tags: ["test2", "integration2"]
