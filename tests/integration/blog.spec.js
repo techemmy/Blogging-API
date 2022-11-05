e5bcf1cd9074d94a2c5a61c3a5f0ab5f60e328b4
Emmanuel Oloyede <emmanueltopea@gmail.com>
2022-11-05 15:19:13 +0100

    test: write test for Blog DELETE '/blogs/:id' endpoint

--- tests/integration/blog.spec.js
+++ tests/integration/blog.spec.js
@@ -516,3 +516,63 @@ describe("Test for Blog PUT '/blogs/:id' request", () => {
         expect(request.body.status).toBeFalsy();
     })
 })
+
+describe("Test for Blog DELETE '/blogs/:id' request", () => {
+    let userToken, userBlog, deletedBlogId, blogByAnotherUser;
+
+    beforeAll(async () => {
+        const user = await User.create(fixtures.userTestData.valid);
+        const user2 = await User.create(fixtures.userTestData.valid2);
+        const request = await supertest(app).post("/auth/login").send(fixtures.userTestData.validLogin);
+
+        const toDelete = await Blog.create({...fixtures.blogTestData.valid3, author: user._id});
+        await Blog.findByIdAndDelete(toDelete._id);
+
+        userBlog = await Blog.create({...fixtures.blogTestData.valid, author: user._id});
+        blogByAnotherUser = await Blog.create({...fixtures.blogTestData.valid2, author: user2._id});
+
+        userToken = request.body.token;
+        deletedBlogId = toDelete._id
+    })
+
+    afterAll(async () => {
+        await database.cleanup()
+    })
+
+    it("should delete blog successfully", async () => {
+        const request = await supertest(app).delete(`/blogs/${userBlog._id}`)
+            .set("Authorization", `Bearer ${userToken}`)
+        expect(request.status).toBe(200);
+        expect(request.headers['content-type']).toContain("application/json");
+        expect(request.body.status).toBeTruthy()
+    })
+
+    it("should fail to delete blog due to missing token header", async () => {
+        const request = await supertest(app).delete(`/blogs/${userBlog._id}`)
+        expect(request.status).toBe(401);
+    })
+
+    it("should fail to delete blog due to invalid blog id", async () => {
+        const request = await supertest(app).delete(`/blogs/invalidBlogId}`)
+            .set("Authorization", `Bearer ${userToken}`)
+        expect(request.status).toBe(400);
+        expect(request.headers['content-type']).toContain("application/json");
+        expect(request.body.status).toBeFalsy()
+    })
+
+    it("should fail to delete blog because blog doesn't exist", async () => {
+        const request = await supertest(app).delete(`/blogs/${deletedBlogId}`)
+            .set("Authorization", `Bearer ${userToken}`)
+        expect(request.status).toBe(404);
+        expect(request.headers['content-type']).toContain("application/json");
+        expect(request.body.status).toBeFalsy()
+    })
+
+    it("should fail to delete blog doesn't belongs to another user", async () => {
+        const request = await supertest(app).delete(`/blogs/${blogByAnotherUser._id}`)
+            .set("Authorization", `Bearer ${userToken}`)
+        expect(request.status).toBe(403);
+        expect(request.headers['content-type']).toContain("application/json");
+        expect(request.body.status).toBeFalsy()
+    })
+})
