7dc37b42520c3b3629c6317ac6843503a601cb82
Emmanuel Oloyede <emmanueltopea@gmail.com>
2022-11-06 23:19:04 +0100

    buid: change environment variable from API_PORT to PORT

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const app = require("./index");
 const { connectToMongoDb } = require("./database");
 require("dotenv").config();
 
-const API_PORT = process.env.API_PORT || 3000;
+const API_PORT = process.env.PORT || 3000;
 const DB_URI = process.env.DB_URI;
 
 connectToMongoDb(DB_URI);
