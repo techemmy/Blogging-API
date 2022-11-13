8504e8a932b4f7908ff5e3a69d0b54599f972d21
Emmanuel Oloyede <emmanueltopea@gmail.com>
2022-11-13 20:40:06 +0100

    feat: add swagger documentation for /blogs endpoint

--- index.js
+++ index.js
@@ -2,11 +2,43 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const helmet = require("helmet");
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
 const authRoutes = require("./routes/auth.routes");
 const blogRoutes = require("./routes/blog.routes");
 
 const app = express();
 app.disable("x-powered-by");
+
+const options = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "Blogging API - Emmanuel Oloyede",
+            version: "0.1.0",
+            description:
+                "This is a simple API application made with Express and documented with Swagger",
+            contact: {
+                name: "TechEmmy",
+                url: "https://github.com/techemmy",
+                email: process.env.EMAIL_ADDRESS,
+            },
+        },
+        servers: [
+            {
+                url: "http://localhost:8000/blogs",
+            },
+        ],
+    },
+    apis: ["./routes/blog.routes.js"],
+};
+
+const specs = swaggerJsdoc(options);
+app.use(
+    "/api-docs",
+    swaggerUi.serve,
+    swaggerUi.setup(specs, { explorer: true })
+);
 
 app.use(morgan("dev"));
 app.use(helmet());
@@ -15,31 +47,31 @@ app.use(bodyParser.urlencoded({ extended: true }));
 require("./middleware");
 
 app.get("/", (req, res) => {
-	res.status(200).json({ message: "Homepage" });
+    res.status(200).json({ message: "Homepage" });
 });
 
 app.use("/auth", authRoutes);
 app.use("/blogs", blogRoutes);
 
 app.use((req, res, next) => {
-	try {
-		res.status(404).json({ error: "Route doesn't exist!" });
-	} catch (error) {
-		next(error);
-	}
+    try {
+        res.status(404).json({ error: "Route doesn't exist!" });
+    } catch (error) {
+        next(error);
+    }
 });
 
 // error Handler middleware
 app.use((error, req, res, next) => {
-	try {
-		if (error.name === "ValidationError") {
-			//  sets status code for mongoose validation error
-			error.status = 400;
-		}
-		res.status(error.status || 500).json({ error: error.message });
-	} catch (error) {
-		console.log(error);
-	}
+    try {
+        if (error.name === "ValidationError") {
+            //  sets status code for mongoose validation error
+            error.status = 400;
+        }
+        res.status(error.status || 500).json({ error: error.message });
+    } catch (error) {
+        console.log(error);
+    }
 });
 
 module.exports = app;
--- routes/blog.routes.js
+++ routes/blog.routes.js
@@ -4,32 +4,206 @@ const authController = require("../controllers/blogController");
 
 const router = express.Router();
 
+/**
+ *  @swagger
+ *   components:
+ *     schemas:
+ *       Blog:
+ *         type: object
+ *         required:
+ *           - title
+ *           - body
+ *           - author
+ *           - state
+ *         properties:
+ *           id:
+ *             type: integer
+ *             description: The auto-generated id of the blog.
+ *           title:
+ *             type: string
+ *             description: The title of your blog.
+ *           description:
+ *             type: string
+ *             description: The description of your blog.
+ *           body:
+ *             type: string
+ *             description: The body of your blog.
+ *           author:
+ *             type: string
+ *             description: Who wrote the blog?
+ *           state:
+ *             type: string
+ *             enum: ["draft", "published"]
+ *             description: Is the blog in drafts or published?
+ *           read_count:
+ *             type: number
+ *             description: The amount of time the blog has been read
+ *           reading_time:
+ *             type: object
+ *             description: How long would it take to read the blog?
+ *             properties:
+ *               inString:
+ *                    type: string
+ *                    description: auto-generated time it would take to read blog in strings as minutes and seconds
+ *               inNumber:
+ *                    type: number
+ *                    description: auto-generated time it would take to read blog in number seconds
+ *           tags:
+ *             type: array
+ *             description: The blog tags
+ *           createdAt:
+ *             type: string
+ *             format: date
+ *             description: The auto-generated date of the record creation.
+ *           updatedAt:
+ *             type: string
+ *             format: date
+ *             description: The auto-generated date of the record update.
+ *         example:
+ *            title: G.O.A.T
+ *            description: Becoming the GOAT 🐐
+ *            body: He he! Are you sure?
+ *            tags: goat,football
+ */
+
+/**
+ *  @swagger
+ *  tags:
+ *    name: Blogs
+ *    description: API to manage your blogs.
+ */
+
+/**
+ * components:
+ *   securitySchemes:
+ *     bearerAuth:
+ *       type: http
+ *       scheme: bearer
+ *       bearerFormat: JWT
+ */
+
+/**
+ *  @swagger
+ * components:
+ *   securitySchemes:
+ *     bearerAuth:
+ *       type: http
+ *       scheme: bearer
+ *       bearerFormat: JWT
+ * paths:
+ *   /:
+ *     get:
+ *       summary: Gets all published books
+ *       tags: [Blogs]
+ *       responses:
+ *         "200":
+ *           description: The published books
+ *     post:
+ *       security:
+ *         - bearerAuth: []
+ *       summary: Create a new blog
+ *       tags: [Blogs]
+ *       requestBody:
+ *         required: true
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Blog'
+ *       responses:
+ *         "201":
+ *           description: Blog created successfully
+ *
+ *   /mine:
+ *     get:
+ *       security:
+ *         - bearerAuth: []
+ *       summary: Gets current logged in user blogs
+ *       tags: [Blogs]
+ *       responses:
+ *         "200":
+ *           description: Current user blogs
+ *
+ *   /publish/{id}:
+ *     patch:
+ *       security:
+ *         - bearerAuth: []
+ *       summary: Change blog state to published
+ *       tags: [Blogs]
+ *       parameters:
+ *         - in: path
+ *           name: id
+ *           description: ID of blog to publish
+ *           type: string
+ *           required: true
+ *       responses:
+ *         "200":
+ *           description: Blog published successfully
+ *
+ *   /{blogId}:
+ *     put:
+ *       security:
+ *         - bearerAuth: []
+ *       summary: Edit a blog
+ *       tags: [Blogs]
+ *       parameters:
+ *         - in: path
+ *           name: blogId
+ *           description: ID of blog to edit
+ *           type: string
+ *           required: true
+ *       requestBody:
+ *         required: true
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Blog'
+ *       responses:
+ *         "200":
+ *           description: Blog edited successfully
+ *
+ *   /{blogID}:
+ *     delete:
+ *       security:
+ *         - bearerAuth: []
+ *       summary: Delete a blog
+ *       tags: [Blogs]
+ *       parameters:
+ *         - in: path
+ *           name: blogID
+ *           description: ID of blog to delete
+ *           type: string
+ *           required: true
+ *       responses:
+ *         "200":
+ *           description: Blog deleted successfully
+ */
+
 router.get("/", authController.getAllPublishedBlogs_get);
 router.get(
-	"/mine",
-	passport.authenticate("jwt", { session: false }),
-	authController.getUserBlogs_get
+    "/mine",
+    passport.authenticate("jwt", { session: false }),
+    authController.getUserBlogs_get
 );
 router.get("/:id", authController.getPublishedBlogById_get);
 router.post(
-	"/",
-	passport.authenticate("jwt", { session: false }),
-	authController.createBlog_post
+    "/",
+    passport.authenticate("jwt", { session: false }),
+    authController.createBlog_post
 );
 router.patch(
-	"/publish/:id",
-	passport.authenticate("jwt", { session: false }),
-	authController.updateBlogToPublish_patch
+    "/publish/:id",
+    passport.authenticate("jwt", { session: false }),
+    authController.updateBlogToPublish_patch
 );
 router.put(
-	"/:id",
-	passport.authenticate("jwt", { session: false }),
-	authController.editBlog_put
+    "/:id",
+    passport.authenticate("jwt", { session: false }),
+    authController.editBlog_put
 );
 router.delete(
-	"/:id",
-	passport.authenticate("jwt", { session: false }),
-	authController.deleteBlog_delete
+    "/:id",
+    passport.authenticate("jwt", { session: false }),
+    authController.deleteBlog_delete
 );
 
 module.exports = router;
